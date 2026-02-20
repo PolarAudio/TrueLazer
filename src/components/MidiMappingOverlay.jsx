c6b913a10ad08ff85840fef52df68439f2653d25
MAXQON <216683641+PolarAudio@users.noreply.github.com>
2026-02-20 14:47:40 +0800

    fix(midi): Resolve JSX syntax error in MidiMappingOverlay

--- src/components/MidiMappingOverlay.jsx
+++ src/components/MidiMappingOverlay.jsx
@@ -335,7 +335,6 @@ const MidiMappingOverlay = () => {
                                         )}
                                     </div>
                                 </div>
-                                </div>
                             </div>
                         ))
                     ) : (
